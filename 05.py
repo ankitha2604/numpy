88667df1c676b58ecc09e4595d20aa3615633f48
ankitha2604 <ankithamurthyk2024@gmail.com>
2025-07-28 12:37:48 +0530

    Add files via upload

---
+++ 05.py
@@ -0,0 +1,14 @@
+#array in numpy
+import numpy as np
+a=np.array([1,2,3])
+b=np.array([4,5,6])
+sum=a + b
+product=a * b
+mean=np.mean(a)
+max=np.max(b)
+print("array a:",a)
+print("array b:",b)
+print("Sum:",sum)
+print("Product:",product)
+print("Mean:",mean)
+print("Maxmium:",max)
---
+++ 07.py
@@ -0,0 +1,14 @@
+#array in numpy
+import numpy as np
+a=np.array([[1,2,3],[4,5,7]])
+b=np.array([[4,5,6],[9,1,2]])
+minus=a-b
+division=a/b
+mean=np.mean(a)
+min=np.min(b)
+print("array a:",a)
+print("array b:",b)
+print("minus:",minus)
+print("division:",division)
+print("mean:",mean)
+print("Minimum:",min)
